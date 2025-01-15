bb2f1ce1c235433f00604ed04ef808cae62f8858
nickrukos <nickrukos@mail.ru>
2025-01-15 23:53:27 +0300

    Домашнее задание к уроку 11 Проект notifier

---
+++ src/main/java/com/itmo/notifier/controller/NotificationController.java
@@ -0,0 +1,11 @@
+package com.itmo.notifier.controller;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/notifier")
+public class NotificationController {
+
+}
